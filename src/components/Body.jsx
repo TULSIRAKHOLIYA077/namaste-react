21b9dc01f95df3dce8075586c1499198857ddd14
TULSIRAKHOLIYA077 <radhikarakholiya077@gmail.com>
2024-11-15 18:15:33 +0530

    code before adding gricery feature

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,6 +2,7 @@ import { RestaurantCard} from "./RestaurantCard";
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () =>{
   const [listOfRestaurant, setListOfRestaurant] = useState([]);
@@ -31,6 +32,13 @@ const Body = () =>{
   // if(listOfRestaurant.length === 0){
   //   return <Shimmer/>
   // }
+
+  const onlineStatus = useOnlineStatus();
+
+  if(onlineStatus == false) 
+    return (
+      <h1>Looks like you are offline!! Please check your internet connection</h1>
+    );
 
   return (listOfRestaurant.length === 0) ? <Shimmer/> :(
     <div className="body">
